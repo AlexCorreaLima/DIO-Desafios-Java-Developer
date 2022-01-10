35f7bfabe2b6c55a1efbf904da4b67cac6210347
Alex Sander Correa Lima <68969503+AlexCorreaLima@users.noreply.github.com>
2022-01-10 16:09:35 -0300

    Create Desafio1.java

---
+++ Desafio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Desafio1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int L = sc.nextInt();
+		int C = sc.nextInt();
+		if ((L+C) % 2 == 0)
+			System.out.println("1");
+		else                                               //complete o código nos espaços em branco
+			System.out.println("0");
+		sc.close();
+	}
+} 
